5502a19717d7b67511ab1967861215ed5fbfd860
ZeratoXD <ivan_cassio_dias@hotmail.com>
2024-04-12 11:41:27 -0300

    CRUD-Categoria

--- src/main/java/com/generation/farmacia/repository/ProdutoRepository.java
+++ src/main/java/com/generation/farmacia/repository/ProdutoRepository.java
@@ -1,5 +1,14 @@
 package com.generation.farmacia.repository;
 
-public interface ProdutoRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.farmacia.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	 List<Produto> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
 }
